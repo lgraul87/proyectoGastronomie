69cae2ffca5713102002745ba2d157d2d2b3611c
Raul Lora Gomez <lgraul87@gmail.com>
2020-04-22 03:29:19 +0200

    Instalacion

---
+++ src/modelo/IInstalacion.java
@@ -0,0 +1,31 @@
+package modelo;
+
+public interface IInstalacion {
+	public final int MAX_CHAR_NOMBRE = 30;
+	public final int MAX_CHAR_DESCRIPCION = 100;
+	public final int MAX_CHAR_DIRECCION = 40;
+	public final float MAX_CUOTA_MENSUAL = 3000;
+	public final float MIN_CUOTA_MENSUAL = 0;
+
+	public String getsNombreInstalacion();
+
+	public String getsDescripcion();
+
+	public boolean setsDescripcion(String sDescripcion);
+
+	public String getsDireccion();
+
+	public boolean setsDireccion(String sDireccion);
+
+	public float getfCuotaMensual();
+
+	public boolean setfCuotaMensual(float fCuotaMensual);
+
+	public boolean validarInstalacion();
+
+	public int hashCode();
+
+	public boolean equals(Object obj);
+
+	public String toString();
+}
--- src/modelo/Instalacion.java
+++ src/modelo/Instalacion.java
@@ -1,5 +1,126 @@
 package modelo;
 
-public class Instalacion {
+public class Instalacion implements IInstalacion {
+
+	private String sNombreInstalacion; // PK
+	private String sDescripcion;
+	private String sDireccion;
+	private float fCuotaMensual;
+
+	public Instalacion(String sNombreInstalacion, String sDireccion, float fCuotaMensual) {
+		setsNombreInstalacion(sNombreInstalacion);
+		setsDireccion(sDireccion);
+		setfCuotaMensual(fCuotaMensual);
+
+	}
+
+	public Instalacion(String sNombreInstalacion, String sDescripcion, String sDireccion, float fCuotaMensual) {
+		setsNombreInstalacion(sNombreInstalacion);
+		setsDescripcion(sDescripcion);
+		setsDireccion(sDireccion);
+		setfCuotaMensual(fCuotaMensual);
+	}
+
+	@Override
+	public String getsNombreInstalacion() { // PK
+		return this.sNombreInstalacion;
+	}
+
+	private boolean setsNombreInstalacion(String sNombreInstalacion) {
+		boolean bValido = false;
+		if (sNombreInstalacion != null && sNombreInstalacion.length() <= MAX_CHAR_NOMBRE) {
+			this.sNombreInstalacion = sNombreInstalacion;
+			bValido = true;
+		}
+		return bValido;
+	}
+
+	@Override
+	public String getsDescripcion() {
+		return this.sDescripcion;
+	}
+
+	@Override
+	public boolean setsDescripcion(String sDescripcion) {
+		boolean bValido = false;
+		if (sDescripcion != null && sDescripcion.length() <= MAX_CHAR_DESCRIPCION) {
+			this.sDescripcion = sDescripcion;
+			bValido = true;
+		}
+		return bValido;
+	}
+
+	@Override
+	public String getsDireccion() {
+		return this.sDireccion;
+	}
+
+	@Override
+	public boolean setsDireccion(String sDireccion) {
+		boolean bValido = false;
+		if (sDireccion != null && sDireccion.length() <= MAX_CHAR_DIRECCION) {
+			this.sDireccion = sDireccion;
+			bValido = true;
+		}
+		return bValido;
+	}
+
+	@Override
+	public float getfCuotaMensual() {
+		return this.fCuotaMensual;
+	}
+
+	@Override
+	public boolean setfCuotaMensual(float fCuotaMensual) {
+		boolean bValido = false;
+		if (fCuotaMensual <= MAX_CUOTA_MENSUAL && fCuotaMensual >= MIN_CUOTA_MENSUAL) {
+			this.fCuotaMensual = fCuotaMensual;
+			bValido = true;
+		} else {
+			this.fCuotaMensual = -1;
+		}
+		return bValido;
+	}
+
+	@Override
+	public boolean validarInstalacion() {
+		boolean bValido = false;
+		if (this.sNombreInstalacion != null && this.sDireccion != null && this.fCuotaMensual != -1) {
+			bValido = true;
+		}
+		return bValido;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((sNombreInstalacion == null) ? 0 : sNombreInstalacion.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		boolean bIgual = false;
+		Instalacion other = (Instalacion) obj;
+		if (this.sNombreInstalacion.equals(other.sNombreInstalacion)) {
+			bIgual = true;
+		}
+		return bIgual;
+	}
+
+	@Override
+	public String toString() {
+		return "Instalacion: " + this.sNombreInstalacion + " ."
+		//
+				+ "  --Descripcion: " + this.sDescripcion
+				//
+				+ "  --Direccion: " + this.sDireccion
+				//
+				+ "  --Cuota Mensual: " + this.fCuotaMensual
+				//
+				+ "\n-------------------------------------------------------------------------------------\n";
+		//
+	}
 
 }
