4086c343f46928a8b1d81dc1c7756324d4871e88
Raul Lora Gomez <lgraul87@gmail.com>
2020-04-30 21:48:33 +0200

    Modelo Producto

---
+++ src/modelo/IProducto.java
@@ -0,0 +1,23 @@
+package modelo;
+
+public interface IProducto {
+	
+	public String getsNombreProducto();
+
+	public float getfPrecio();
+
+	public boolean setfPrecio(float fPrecio);
+
+	public short getShStock();
+
+	public boolean setShStock(short shStock);
+
+	public boolean checkProducto();
+
+	public int hashCode();
+
+	public boolean equals(Object obj);
+
+	public String toString();
+
+}
--- src/modelo/Producto.java
+++ src/modelo/Producto.java
@@ -1,5 +1,95 @@
 package modelo;
 
-public class Producto {
+public class Producto implements IProducto, LimitsDB {
+	private String sNombreProducto; // PK
+	private float fPrecio; // NN
+	private short shStock; // NN
+
+	public Producto(String sNombreProducto, float fPrecio, short shStock) {
+
+		setsNombreProducto(sNombreProducto);
+		setfPrecio(fPrecio);
+		setShStock(shStock);
+	}
+
+	@Override
+	public String getsNombreProducto() {
+		return this.sNombreProducto;
+	}
+
+	private boolean setsNombreProducto(String sNombreProducto) { // PK
+		boolean bValido = false;
+		if (sNombreProducto != null && sNombreProducto.length() <= MAX_CHAR_15) {
+			this.sNombreProducto = sNombreProducto;
+			bValido = true;
+		}
+		return bValido;
+	}
+
+	@Override
+	public float getfPrecio() {
+		return this.fPrecio;
+	}
+
+	@Override
+	public boolean setfPrecio(float fPrecio) {
+		boolean bValido = false;
+		if (fPrecio > MIN_FLOAT_0 && fPrecio <= MAX_FLOAT_3000) {
+			this.fPrecio = fPrecio;
+			bValido = true;
+		} else {
+			this.fPrecio = -1;
+		}
+		return bValido;
+	}
+
+	@Override
+	public short getShStock() {
+		return this.shStock;
+	}
+
+	@Override
+	public boolean setShStock(short shStock) {
+		boolean bValido = false;
+		if (shStock > MIN_INT_0 && shStock <= MAX_INT_100) {
+			this.shStock = shStock;
+			bValido = true;
+		} else {
+			this.shStock = -1;
+		}
+		return bValido;
+	}
+
+	@Override
+	public boolean checkProducto() {
+		boolean bValido = false;
+		if (this.sNombreProducto != null && this.fPrecio != -1 && this.shStock != -1) {
+			bValido = true;
+		}
+		return bValido;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((sNombreProducto == null) ? 0 : sNombreProducto.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		boolean bIgual = false;
+		Producto other = (Producto) obj;
+		if (this.checkProducto() && other.checkProducto() && this.sNombreProducto.equals(other.sNombreProducto)) {
+			bIgual = true;
+		}
+		return bIgual;
+	}
+
+	@Override
+	public String toString() {
+		return "  --Material: " + this.sNombreProducto + "  --Precio: " + this.fPrecio + "  --Stock: " + this.shStock;
+	}
 
 }
